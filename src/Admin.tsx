3b32f56d48eb8ad0cac292c9bc083f27ddb20245
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-08-01 14:52:17 +0200

    improve layout

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -8,14 +8,14 @@ export default function Admin() {
   const pieceRefs = usePieceRefs();
   return (
     <>
-      <div className="h-full grid grid-cols-[auto_auto] lt-sm:grid-cols-1 gap-x-4 gap-y-8 lt-sm:gap-y-4 justify-items-center mt-6">
+      <div className="h-full grid grid-cols-[auto_auto] lt-sm:grid-cols-1 gap-x-4 gap-y-8 lt-sm:gap-y-4 justify-items-center my-6">
         {Object.entries(pieceRefs).map(([color, ref]) => {
           const url = `${window.location.origin}${
             import.meta.env.BASE_URL
           }?add=${color}`;
           return (
             <Fragment key={color}>
-              <div className="prose h-full lt-sm:w-auto lt-sm:w-full rounded-lg bg-gray-800 p-2 lt-sm:mb-0">
+              <div className="prose h-full lt-sm:w-auto lt-sm:w-full rounded-lg bg-gray-800 p-2 lt-sm:mb-0 justify-center">
                 <div ref={ref} className="w-20 h-20" />
                 <a href={url}>Add {color}</a>
               </div>
