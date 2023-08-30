68fcc4c4d3ce01f87ba75bcf4278fdf55d04edd0
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-08-30 09:02:22 +0200

    improve popover layout

--- src/Popover.tsx
+++ src/Popover.tsx
@@ -6,13 +6,13 @@ export default function Popover({
   imageSource: string;
 }) {
   return (
-    <div className="max-w-screen sm:max-w-screen-sm p-4 h-full justify-center items-center text-xl bg-gray-800 bg-opacity-85">
+    <div className="w-full p-4 h-full justify-center items-center text-xl bg-gray-800 bg-opacity-85">
       <div className="bg-black rounded-md shadow-gray-300 shadow-xl border-3 border-gray-700 bg-opacity-60">
         {imageSource && (
           <img className="p-2 object-contain" src={imageSource} />
         )}
         {message && <div className="p-6 text-center">{message}</div>}
-        <div className="text-sm italic text-gray-300">
+        <div className="text-sm italic text-gray-300 px-4">
           click anywhere to close
         </div>
       </div>
