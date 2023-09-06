39ca41374953dd31bc86b05e84d7ff0175502238
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-09-06 15:29:30 -0700

    incorporate nf logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,5 @@
 import logo from './assets/logo.svg';
+import neaformLogo from './assets/nf-logo.svg';
 
 export default function Header() {
   return (
@@ -7,14 +8,20 @@ export default function Header() {
         <div className="flex-shrink flex-grow">
           <img
             src={logo}
-            className="w-full max-h-[clamp(3rem,12vw,3.8rem)] min-w-[60px]"
+            className="w-full max-h-[clamp(2.25rem,10vw,3.8rem)] min-w-[60px]"
           />
         </div>
-        <div className="font-heading text-size-[clamp(1.5rem,6vw,1.9rem)] line-height-none items-start flex-grow uppercase">
+        <div className="font-heading text-size-[clamp(1.125rem,5vw,1.9rem)] line-height-none items-start flex-grow uppercase">
           <div className="font-normal whitespace-nowrap tracking-tighter">
             Grace Hopper
           </div>
           <div className="font-bold tracking-wide">Celebration</div>
+        </div>
+        <div className="flex-shrink flex-grow">
+          <img
+            className="w-full max-h-[clamp(2.25rem,10vw,3.8rem)] min-w-[40px] p-[3px]"
+            src={neaformLogo}
+          />
         </div>
       </div>
     </div>
