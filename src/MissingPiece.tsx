1a040401204825a035dbf7e0d829a8519af94c8c
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-09-25 20:40:11 +0200

    fix hiring line

--- src/MissingPiece.tsx
+++ src/MissingPiece.tsx
@@ -1,14 +1,14 @@
 export default function MissingPiece() {
   return (
     <div className="relative text-xs items-center text-center bg-white text-dark-violet w-full inline-block text-center py-1">
-      Are you the missing piece? Build you @{' '}
+      <span className="font-bold">We&apos;re hiring!</span> Are you the missing
+      piece? View{' '}
       <a
         className="font-bold underline"
         href="https://www.nearform.com/careers/"
       >
         NearForm careers
       </a>
-      .
     </div>
   );
 }
