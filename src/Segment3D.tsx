07d010db2c61259adc6fb2fe55b2294c6a232fb5
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-10-20 14:09:07 +0200

    Fix resetting highlight animation on new piece

--- src/Segment3D.tsx
+++ src/Segment3D.tsx
@@ -1,4 +1,5 @@
 import {useSpring, animated} from '@react-spring/three';
+import {useEffect} from 'react';
 import {type PieceColor} from './piece-types';
 
 export default function Segment3D({
@@ -14,43 +15,55 @@ export default function Segment3D({
   y: number;
   z: number;
 }) {
-  const [{scale: highlightScale, color: highlightColor}] = useSpring(
+  const [{scale: highlightScale, color: highlightColor}, api] = useSpring(
     () => ({
-      from: {scale: 0.92, color},
-      to: [
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-        {scale: 1.2, color: '#ffffff'},
-        {scale: 0.92, color},
-      ],
-      reset: !highlight,
+      scale: 0.92,
+      // Keep TS happy
+      color: color as PieceColor | '#ffffff',
       config: {
         duration: 300,
       },
     }),
-    [highlight],
+    [],
   );
+
+  useEffect(() => {
+    if (highlight) {
+      api.start({
+        to: [
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+          {scale: 1.2, color: '#ffffff'},
+          {scale: 0.92, color},
+        ],
+      });
+    } else {
+      api.stop();
+      api.set({
+        scale: 0.92,
+        color,
+      });
+    }
+  }, [highlight, api, color]);
 
   return (
     <animated.mesh
       key={`cube-${y}-${x}`}
       position={[x, y, z]}
-      scale={highlight ? highlightScale : 0.92}
+      scale={highlightScale}
     >
       {/* eslint-disable-next-line react/no-unknown-property */}
       <boxGeometry args={[1, 1, 1]} />
       {/* @ts-expect-error TS does not like the animated MeshStandardMaterial */}
-      <animated.meshStandardMaterial
-        color={highlight ? highlightColor : color}
-      />
+      <animated.meshStandardMaterial color={highlightColor} />
     </animated.mesh>
   );
 }
